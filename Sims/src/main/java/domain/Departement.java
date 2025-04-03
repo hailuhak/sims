27a67f5d29cc67fdb96bd2285f62526835ed957a
nard0s <ninanteneh20@gmail.com>
2025-04-03 10:40:40 +0300

    department updated

--- Sims/src/main/java/domain/Departement.java
+++ Sims/src/main/java/domain/Departement.java
@@ -27,4 +27,10 @@ public class Departement {
         this.name = name;
     }
     String name;
+    
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    String name;
 }
